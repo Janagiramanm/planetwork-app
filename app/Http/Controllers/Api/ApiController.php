5aa579b6dca6c57cb5c9fdebf76bf39d4c48cf4b
Janagiramanm <janagiraman@netiapps.com>
2022-05-10 18:29:33 +0530

    debug

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -72,6 +72,7 @@ class ApiController extends Controller
         //    $distance_km = 0.0;
            $travel = 0 ;
            if($data){
+            
             // $insert = TrackLocations::insert($data);
                foreach($data as $key => $value){
                     $track = new TrackLocations();
@@ -95,9 +96,12 @@ class ApiController extends Controller
                     // echo '<pre>';
                     // print_r($report);
                     if($report){
-                        $distance =  $this->calculateDistanceBetweenTwoPoints($report->to_lat,$report->to_lng,$value['latitude'],$value['longitude']);
+                       $distance =  $this->calculateDistanceBetweenTwoPoints($report->to_lat,$report->to_lng,$value['latitude'],$value['longitude']);
+                      
+
                         if($report->job_id == $value['job_id']){
                             $travelDistance = $report->travel_distance + $distance;
+                            print_r($travelDistance);
                             $workReport = WorkReport::find($report->id);
                             $workReport->travel_distance = (float)$travelDistance;
                             $workReport->to_lat = $value['latitude'];
@@ -108,6 +112,12 @@ class ApiController extends Controller
                                $workReport->is_reached = 'true';
                             }
                             $workReport->save();
+                            // $query = DB::getQueryLog();
+                            // print_r($workReport);
+                            // exit;
+                            // dd(DB::getQueryLog());
+                            // exit;
+                            // dd(DB::getQueryLog());
                         }else{
                             $workReport = new WorkReport();
                             $workReport->date = $value['date'];
@@ -209,7 +219,6 @@ class ApiController extends Controller
        $user_id = $request->user_id;
       //     $user_id = 2;
 
-
       $res = WorkReport::where('user_id','=', $user_id)
       ->whereBetween('date',[$from_date, $to_date])
       ->get();
@@ -217,85 +226,30 @@ class ApiController extends Controller
         $dateWiseData = [];
         if($res){
 
-        foreach($res as $key => $value){
-            $dateWiseData[$key]['date'] = $value->date;
-            
-            $dateWiseData[$key]['customer_name'] = '';
-            $dateWiseData[$key]['job_name'] = '';
-            $dateWiseData[$key]['job_status'] = '';
-            $dateWiseData[$key]['sr_no'] = '';
+                    foreach($res as $key => $value){
+                        $dateWiseData[$key]['date'] = $value->date;
+                        
+                        $dateWiseData[$key]['customer_name'] = '';
+                        $dateWiseData[$key]['job_name'] = '';
+                        $dateWiseData[$key]['job_status'] = '';
+                        $dateWiseData[$key]['sr_no'] = '';
 
-            $dateWiseData[$key]['user_id'] = $value->user_id;
-            $dateWiseData[$key]['user_name'] = $value->user->name;
-            if($value->job_id != 0){
-                $dateWiseData[$key]['customer_name'] = $value->job->customer->first_name;
-                $dateWiseData[$key]['job_name'] = $value->job->task->name;
-                $dateWiseData[$key]['job_status'] = $value->job->employees;
-                $dateWiseData[$key]['sr_no'] = $value->job->sr_no;
-                $dateWiseData[$key]['travel_distance'] = $value->travel_distance;
-                $dateWiseData[$key]['from_address'] = $this->getAddress($value->from_lat,$value->from_lng);
-                $dateWiseData[$key]['to_address'] = $this->getAddress($value->to_lat,$value->to_lng);
-                $dateWiseData[$key]['start'] = $value->start;
-                $dateWiseData[$key]['end'] = $value->end;
-            }
+                        $dateWiseData[$key]['user_id'] = $value->user_id;
+                        $dateWiseData[$key]['user_name'] = $value->user->name;
+                        if($value->job_id != 0){
+                            $dateWiseData[$key]['customer_name'] = $value->job->customer->first_name;
+                            $dateWiseData[$key]['job_name'] = $value->job->task->name;
+                            $dateWiseData[$key]['job_status'] = $value->job->employees;
+                            $dateWiseData[$key]['sr_no'] = $value->job->sr_no;
+                            $dateWiseData[$key]['travel_distance'] = $value->travel_distance;
+                            $dateWiseData[$key]['from_address'] = $this->getAddress($value->from_lat,$value->from_lng);
+                            $dateWiseData[$key]['to_address'] = $this->getAddress($value->to_lat,$value->to_lng);
+                            $dateWiseData[$key]['start'] = $value->start;
+                            $dateWiseData[$key]['end'] = $value->end;
+                        }
+                    }
+
         }
-
-        }
-        // $this->result = $dateWiseData;
-
-                         
-    //   $result = DB::table('track_locations')
-    //   ->select(DB::raw('DATE(date) as date'))
-    //   ->where('user_id','=', $user_id)
-    //   ->groupBy('date')
-    //   ->get();
-
-     
-    //   $final_result = [];
-    //    if($result){
-    //        $dateWiseData = [];
-    //        foreach($result as $key => $value){
-
-    //             // $a = TrackLocations::where('user_id', '=', $user_id)
-    //             //      ->where('date', '=' , $value->date)->orderBy('id','asc')->limit(1);
-
-    //             // $dateWiseData[$value->date] = TrackLocations::where('user_id', '=', $user_id)
-    //             //      ->where('date', '=' , $value->date)->orderBy('id','desc')->limit(1)->union($a)->get();
-
-
-    //             // $table = DB::table("track_locations")
-    //             //         ->select('*')->where('date', '=' , $value->date)->orderBy('id','asc')->skip(0)->limit(1);
-
-    //             // $dateWiseData[$value->date] =       DB::table("track_locations")
-    //             //         ->select('*')
-    //             //         ->union($table)
-    //             //         ->where('date', '=' , $value->date)->orderBy('id','desc')->skip(0)->limit(1)
-    //             //         ->get();
-
-    //             $dateWiseData[$value->date] =  DB::select('(SELECT * FROM track_locations where user_id = '.$user_id.' and date = "'.$value->date.'" ORDER BY id LIMIT 1)
-    //                                                         UNION ALL
-    //                                                         (SELECT * FROM track_locations where user_id = '.$user_id.' and date = "'.$value->date.'" ORDER BY id DESC LIMIT 1)');
-    //        }
-
-    //        if(!empty($dateWiseData)){
-    //            foreach($dateWiseData as $key => $value){
-
-    //                 $final_result[] = [
-    //                            'date' => $value[0]->date,
-    //                            'start_time' =>  $value[0]->time,
-    //                            'end_time' => $value[1]->time,
-    //                            'from_lat' => $value[0]->latitude,
-    //                            'from_lng' => $value[0]->longitude,
-    //                            'to_lat' => $value[1]->latitude,
-    //                            'to_lng' => $value[1]->longitude,
-    //                            'distance' => round($this->calculateDistanceBetweenTwoPoints($value[0]->latitude,$value[0]->longitude, $value[1]->latitude, $value[1]->longitude, 'KM'),1)
-    //                 ];
-    //            }
-    //        }
-    //    }
-      
-      
-       
         if( empty($dateWiseData)){
             return [
                 'status' => 0,
@@ -446,11 +400,9 @@ class ApiController extends Controller
     {
         if (empty($decimalPoints)) 
         {
-            $decimalPoints = '3';
+            $decimalPoints = 3;
         }
-        if (empty($distanceUnit)) {
-            $distanceUnit = 'KM';
-        }
+       
         $distanceUnit = strtolower($distanceUnit);
         $pointDifference = $longitudeOne - $longitudeTwo;
         $toSin = (sin(deg2rad($latitudeOne)) * sin(deg2rad($latitudeTwo))) + (cos(deg2rad($latitudeOne)) * cos(deg2rad($latitudeTwo)) * cos(deg2rad($pointDifference)));
@@ -459,40 +411,14 @@ class ApiController extends Controller
 
         $toMiles  =  $toRad2Deg * 60 * 1.1515;
         $toKilometers = $toMiles * 1.609344;
-        $toNauticalMiles = $toMiles * 0.8684;
-        $toMeters = $toKilometers * 1000;
-        $toFeets = $toMiles * 5280;
-        $toYards = $toFeets / 3;
+         if(is_float($toKilometers)){
+            $toKilometers  = ($round == true ? round($toKilometers) : round($toKilometers, $decimalPoints));
+         }else{
+              $toKilometers = 0.00;
+         }
+         return $toKilometers;
 
-
-              switch (strtoupper($distanceUnit)) 
-              {
-                  case 'ML'://miles
-                         $toMiles  = ($round == true ? round($toMiles) : round($toMiles, $decimalPoints));
-                         return $toMiles;
-                      break;
-                  case 'KM'://Kilometers
-                        $toKilometers  = ($round == true ? round($toKilometers) : round($toKilometers, $decimalPoints));
-                        return $toKilometers;
-                      break;
-                  case 'MT'://Meters
-                        $toMeters  = ($round == true ? round($toMeters) : round($toMeters, $decimalPoints));
-                        return $toMeters;
-                      break;
-                  case 'FT'://feets
-                        $toFeets  = ($round == true ? round($toFeets) : round($toFeets, $decimalPoints));
-                        return $toFeets;
-                      break;
-                  case 'YD'://yards
-                        $toYards  = ($round == true ? round($toYards) : round($toYards, $decimalPoints));
-                        return $toYards;
-                      break;
-                  case 'NM'://Nautical miles
-                        $toNauticalMiles  = ($round == true ? round($toNauticalMiles) : round($toNauticalMiles, $decimalPoints));
-                        return $toNauticalMiles;
-                      break;
-              }
-
+            
 
     }
 
