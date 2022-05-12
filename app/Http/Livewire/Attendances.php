6326c1bc5075a4618ca41e26bcae1c034e361c78
Janagiramanm <janagiraman@netiapps.com>
2022-05-12 10:40:01 +0530

    debug

--- app/Http/Livewire/Attendances.php
+++ app/Http/Livewire/Attendances.php
@@ -49,9 +49,14 @@ class Attendances extends Component
         $date = $this->year.'-'.$month_number;
         $this->employee = User::find($user_id);
 
-        $this->details = Attendance::where('date','LIKE',$date.'-%')
-        ->where('user_id','=',$user_id)
-        ->get();
+        $this->details = Attendance::groupBy('user_id','date')
+        ->selectRaw('user_id,sum(minutes) as minutes, date')
+        ->where('date','LIKE',$date.'-%')->get();
+
+        // $this->details = Attendance::where('date','LIKE',$date.'-%')
+        // ->where('user_id','=',$user_id)
+        // ->groupBy('date')
+        // ->get();
 
        
     }
