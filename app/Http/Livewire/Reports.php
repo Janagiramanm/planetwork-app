bd502eb44bba20dca4c34df82731cffd8137611c
Janagiramanm <janagiraman@netiapps.com>
2022-05-17 15:13:44 +0530

    debug

--- app/Http/Livewire/Reports.php
+++ app/Http/Livewire/Reports.php
@@ -105,6 +105,9 @@ class Reports extends Component
         
         $dateWiseData = [];
         if($res){
+            echo '<pre>';
+            print_r($res);
+            exit;
 
             foreach($res as $key => $value){
                 $dateWiseData[$key]['date'] = date('d M Y',strtotime($value['created_at']));
